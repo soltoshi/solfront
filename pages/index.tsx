062d52720a1cfc07333efeb718e935fc82e5f359
soltoshi <>
2022-07-24 19:11:00 -0400

    rename index js to tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { createMerchant } from "../state/merchant";
 
 export default function Home() {
+
+  const testCreateMerchant = async () => {
+    await createMerchant('Test Merchant Creation');
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +23,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -52,18 +58,22 @@ export default function Home() {
         </div>
       </main>
 
+      <div>
+        <button onClick={testCreateMerchant}>Test Create Merchant</button>
+      </div>
+
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
