33a438f7456ccdd36e7e096094b9dc825dc260aa
soltoshi <>
2022-07-25 23:39:02 -0400

    feat!: use function params interface for merchant api

--- state/merchant.ts
+++ state/merchant.ts
@@ -5,7 +5,12 @@ import generateDocumentId from "./util/generateDocumentId";
 const COLLECTION_NAME = 'merchants';
 const COLLECTION_PREFIX = 'acct';
 
-const createMerchant = async (name: String) => {
+interface CreateMerchantParams {
+  name: String;
+  email: String;
+}
+
+const createMerchant = async ({name, email}) => {
   try {
     const generatedId = generateDocumentId(COLLECTION_PREFIX);
     await setDoc(doc(db, COLLECTION_NAME, generatedId), {
