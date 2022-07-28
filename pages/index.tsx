528613a0d72476613087cb34f189d3cc4f2903d7
soltoshi <satnak69420@gmail.com>
2022-07-28 00:06:31 -0400

    feat: format list view for payment links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Container, Heading, Wrap, WrapItem } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
 import NavBar from "../components/NavBar";
@@ -15,52 +16,38 @@ export default function Home() {
 
       <NavBar />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-
-        <PaymentLinkCard
-          link="pay.solfront.app/JFOG"
-          productName="Chair"
-          price="39"
-          currency="USD"
-        />
+      <main>
+        <Container maxW='' centerContent={true}>
+          <Heading as='h3' size='lg' marginBottom='32px'>
+            Your payment links
+          </Heading>
+          <Wrap spacing={24} shouldWrapChildren={true} align='center'>
+            <PaymentLinkCard
+              link="pay.solfront.app/JFOG"
+              productName="Chair"
+              price="39"
+              currency="USD"
+            />
+            <PaymentLinkCard
+              link="pay.solfront.app/JFOG"
+              productName="Chair"
+              price="39"
+              currency="USD"
+            />
+            <PaymentLinkCard
+              link="pay.solfront.app/JFOG"
+              productName="Chair"
+              price="39"
+              currency="USD"
+            />
+            <PaymentLinkCard
+              link="pay.solfront.app/JFOG"
+              productName="Chair"
+              price="39"
+              currency="USD"
+            />
+          </Wrap>
+        </Container>
       </main>
 
       <footer className={styles.footer}>
