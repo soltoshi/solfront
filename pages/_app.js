570491184c7a9b2010039af3080f4b9415624b91
soltoshi <satnak69420@gmail.com>
2022-07-29 00:13:03 -0400

    feat: add default doc head in Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Layout>
+        <Head>
+          <title>Solfront</title> <meta name="description" content="Solfront
+          enables your business to effortlessly create payment links, accept
+          cryptocurrency for USD, and incentivize your customers to come back." />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
