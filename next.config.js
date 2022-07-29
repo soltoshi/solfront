c2fa683227ad0f1aa5e72a67fbc4e88a8ddff773
soltoshi <satnak69420@gmail.com>
2022-07-29 01:00:15 -0400

    feat: add rewrite for pay.solfront.app to /pay file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+const rewrites = async () => {
+  // references:
+  // https://vercel.com/support/articles/can-i-redirect-from-a-subdomain-to-a-subpath
+  // https://nextjs.org/docs/api-reference/next.config.js/rewrites
+  return {
+    beforeFiles: [
+    // if the host is `pay.solfront.app`, this rewrite will be applied
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'pay.solfront.app',
+          }
+        ],
+        destination: '/pay/:path*',
+      },
+    ],
+  }
+}
+
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: rewrites,
 }
 
 module.exports = nextConfig
