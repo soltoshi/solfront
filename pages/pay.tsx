64050619fb3f4ef2732c10416ee5e7706644a75a
soltoshi <satnak69420@gmail.com>
2022-07-29 00:48:01 -0400

    feat: mock payment page

---
+++ pages/pay.tsx
@@ -0,0 +1,16 @@
+import { Center, Heading } from "@chakra-ui/react";
+import { NextPage } from "next";
+
+const Pay: NextPage = () => {
+  return (
+    <>
+      <Center>
+        <Heading fontSize={'2xl'}>
+          THIS IS THE PAYMENT PAGE
+        </Heading>
+      </Center>
+    </>
+  )
+}
+
+export default Pay;
