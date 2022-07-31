1330dc676162264726ebefc4858c0d8a16d2a42a
soltoshi <satnak69420@gmail.com>
2022-07-31 02:24:19 -0400

    feat: load payment link details

--- pages/pay.tsx
+++ pages/pay.tsx
@@ -1,15 +1,35 @@
-import { Box, Button, Heading, VStack } from "@chakra-ui/react";
+import { Box, Button, Heading, Spinner, VStack } from "@chakra-ui/react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import PaymentLinkCard from "../components/PaymentLinkCard";
+import { getPaymentLink } from "../state/paymentLink";
+
+// TODO: remove stub
+const LINK_ID = 'pymtlink_AzIdRSYc2C0IX2rPBVWG';
 
 const Pay: NextPage = () => {
-
   const router = useRouter();
+
+  // state that renders
+  const [data, setData] = useState({});
 
   // public key of a connected wallet, if there is one
   const {publicKey} = useWallet();
+
+  // Read the payment link details
+  const loadPaymentLink = async (linkId) => {
+    const data = await getPaymentLink({linkId: linkId});
+    setData(data);
+    console.log(JSON.stringify(data));
+  }
+  useEffect(() => {
+    loadPaymentLink(LINK_ID);
+  }, []);
+
+  const apiParams = {};
 
   return (
     <>
@@ -18,11 +38,28 @@ const Pay: NextPage = () => {
           💸 Pay
         </Heading>
 
-        <Box marginTop={'48px'}>
+        <Box marginTop={'48px!'}>
           <WalletMultiButton />
         </Box>
 
+        {
+          !data.link ?
+           <></> :
+           <PaymentLinkCard
+             link={data.link}
+             productName={data.productName}
+             price={data.productPrice}
+             currency={data.productCurrency}
+
+             offset={true}
+           />
+        }
+
         <Button
+          marginTop={'48px!'}
+          size={'lg'}
+          colorScheme={'green'}
+
           disabled={publicKey === null}
           onClick={() => {
             // router.push(`${router.route}/checkout`);
