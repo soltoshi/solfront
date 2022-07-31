64c385b4052e421fe1fdeebaf836c35fc18d1213
soltoshi <satnak69420@gmail.com>
2022-07-30 23:42:53 -0400

    feat: add wallet and pay button to /pay

--- pages/pay.tsx
+++ pages/pay.tsx
@@ -1,14 +1,30 @@
-import { Center, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, VStack } from "@chakra-ui/react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { NextPage } from "next";
 
 const Pay: NextPage = () => {
+
+  // public key of a connected wallet, if there is one
+  const {publicKey} = useWallet();
+
   return (
     <>
-      <Center>
+      <VStack>
         <Heading fontSize={'2xl'}>
           THIS IS THE PAYMENT PAGE
         </Heading>
-      </Center>
+
+        <Box marginTop={'48px'}>
+          <WalletMultiButton />
+        </Box>
+
+        <Button
+          // disabled={publicKey === null}
+        >
+          Pay
+        </Button>
+      </VStack>
     </>
   )
 }
