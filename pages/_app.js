9e5d266ccf25b611983a8ec5fbda6527e43b621a
soltoshi <satnak69420@gmail.com>
2022-07-30 23:48:39 -0400

    feat: setup wallet connection on _app

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,50 @@ import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 import Head from "next/head";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl } from "@solana/web3.js";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+// Default styles that can be overridden by your app
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 function MyApp({ Component, pageProps }) {
+  // Solana network to use
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = clusterApiUrl(network);
+
+  const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
+
   return (
-    <ChakraProvider>
-      <Layout>
-        <Head>
-          <title>Solfront</title> <meta name="description" content="Solfront
-          enables your business to effortlessly create payment links, accept
-          cryptocurrency for USD, and incentivize your customers to come back." />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <ChakraProvider>
+            <Layout>
+              <Head>
+                <title>Solfront</title>{" "}
+                <meta
+                  name="description"
+                  content="Solfront
+                enables your business to effortlessly create payment links, accept
+                cryptocurrency for USD, and incentivize your customers to come back."
+                />
+                <link rel="icon" href="/favicon.ico" />
+              </Head>
+              <Component {...pageProps} />
+            </Layout>
+          </ChakraProvider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
