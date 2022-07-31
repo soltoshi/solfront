0dd98cfec16ce08715ee566843f02763075138d3
soltoshi <satnak69420@gmail.com>
2022-07-31 00:37:59 -0400

    feat: add usdc devnet addr

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -2,3 +2,5 @@ import { PublicKey } from "@solana/web3.js";
 
 // Solfront's wallet address
 export const shopAddress = new PublicKey('FHedpE3GoRfUjwtDZcc5J1m1dsptzugYFB6c3ZohULFP');
+
+export const usdcDevnetAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr');
