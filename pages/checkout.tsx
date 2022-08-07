973d09f331462531f6667d5778b9240bb728331e
soltoshi <satnak69420@gmail.com>
2022-08-07 18:24:13 -0400

    feat: make and send solana tx to wallet

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,23 +1,115 @@
-import { Box, Button, Heading, VStack } from "@chakra-ui/react";
+import { Box, Heading, VStack } from "@chakra-ui/react";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { Keypair, Transaction } from "@solana/web3.js";
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { MakeTransactionInputData, MakeTransactionOutputData } from "./api/make_transaction";
+import { findReference, FindReferenceError } from "@solana/pay";
+import { usePayContext } from "../context/PayContext";
 
 const Checkout: NextPage = () => {
   const router = useRouter();
-  const {connection} = useConnection();
-  const {publicKey, sendTransaction} = useWallet();
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
 
   // State to hold API response fields
   const [transaction, setTransaction] = useState<Transaction | null>(null);
-  const [message, setMessage] = useState<string | null>(null);
+  const {price, paymentLink} = usePayContext();
 
-  // Generate unique reference that'll be used for this transaction
+  // Generate the unique reference which will be used for this transaction
   const reference = useMemo(() => Keypair.generate().publicKey, []);
+
+  // Use our API to fetch the transaction for the selected items
+  async function getTransaction() {
+    if (!publicKey) {
+      return;
+    }
+
+    const body: MakeTransactionInputData = {
+      account: publicKey.toString(),
+      reference: reference.toString(),
+      price: price,
+      paymentLink: paymentLink,
+    }
+
+    console.log('[checkout] sending /make_transaction request w/body', JSON.stringify(body));
+
+    const response = await fetch(`/api/make_transaction`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+    })
+
+    const json = await response.json() as MakeTransactionOutputData
+
+    if (response.status !== 200) {
+      console.error(json);
+      return;
+    }
+
+    // Deserialize the transaction from the response
+    const transaction = Transaction.from(Buffer.from(json.transaction, 'base64'));
+    setTransaction(transaction);
+    console.log(transaction);
+  }
+
+  useEffect(() => {
+    getTransaction()
+  }, [publicKey])
+
+  // Send the fetched transaction to the connected wallet
+  async function trySendTransaction() {
+    if (!transaction) {
+      return;
+    }
+    try {
+      await sendTransaction(transaction, connection)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  // Send the transaction once it's fetched
+  useEffect(() => {
+    trySendTransaction()
+  }, [transaction])
+
+  // Check every 0.5s if the transaction is completed
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        // Check if there is any transaction for the reference
+        const signatureInfo = await findReference(connection, reference, {finality: 'confirmed'})
+        router.push('/confirmed')
+      } catch (e) {
+        if (e instanceof FindReferenceError) {
+          // No transaction found yet, ignore this error
+          return;
+        }
+        console.error('Unknown error', e)
+      }
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  if (!publicKey) {
+    return (
+      <div className='flex flex-col gap-8 items-center'>
+        <div><Link href='/buy'>Cancel</Link></div>
+
+        <WalletMultiButton />
+
+        <p>You need to connect your wallet to make transactions</p>
+      </div>
+    )
+  }
 
   return (
     <>
