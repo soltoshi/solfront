2f46c17e048d7f2ce3e4370a1c5fe4abff31c72e
soltoshi <satnak69420@gmail.com>
2022-08-08 23:45:12 -0400

    chore: refactor firebase stuff into its own file

---
+++ state/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getApp, getApps, initializeApp } from "firebase/app";
+
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: "G-HH45XES41M",
+};
+
+// Initialize Firebase
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+// const analytics = getAnalytics(app);
+
+export {
+  firebaseConfig
+};
+export default app;
