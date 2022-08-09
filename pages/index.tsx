d495d6c770872f531bec2119f6bb6468048c6d89
soltoshi <satnak69420@gmail.com>
2022-08-09 00:13:05 -0400

    feat: fixup component to get magic link

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,32 +43,19 @@ const Home: NextPage = () => {
               bg={useColorModeValue('white', 'gray.700')}
               boxShadow={'lg'}
               p={8}>
-              <Stack spacing={4}>
+              <Stack spacing={8}>
                 <FormControl id="email">
-                  <FormLabel>Email address</FormLabel>
-                  <Input type="email" />
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email"/>
                 </FormControl>
-                <FormControl id="password">
-                  <FormLabel>Password</FormLabel>
-                  <Input type="password" />
-                </FormControl>
-                <Stack spacing={10}>
-                  <Stack
-                    direction={{ base: 'column', sm: 'row' }}
-                    align={'start'}
-                    justify={'space-between'}>
-                    <Checkbox>Remember me</Checkbox>
-                    <Link color={'blue.400'}>Forgot password?</Link>
-                  </Stack>
-                  <Button
-                    bg={'blue.400'}
-                    color={'white'}
-                    _hover={{
-                      bg: 'blue.500',
-                    }}>
-                    Sign in
-                  </Button>
-                </Stack>
+                <Button
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Get magic link 🪄
+                </Button>
               </Stack>
             </Box>
           </Stack>
