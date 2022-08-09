5127c956bb9de52c33fe6a55ae2cb42640578e39
soltoshi <satnak69420@gmail.com>
2022-08-08 23:55:46 -0400

    feat: copy contents of index to dashboard page

---
+++ pages/dashboard.tsx
@@ -0,0 +1,53 @@
+import { Container, Heading, Wrap} from "@chakra-ui/react";
+import { NextPage } from "next";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import PaymentLinkCard from "../components/PaymentLinkCard";
+import { getPaymentLinks } from "../state/paymentLink";
+
+const Dashboard: NextPage = () => {
+  const [paymentLinks, setPaymentLinks] = useState([]);
+
+  const loadPaymentLinks = async () => {
+    const data = await getPaymentLinks();
+    setPaymentLinks(data);
+  };
+
+  useEffect(() => {
+    loadPaymentLinks();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Solfront</title> <meta name="description" content="Solfront
+        enables your business to effortlessly create payment links, accept
+        cryptocurrency for USD, and incentivize your customers to come back." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Container maxW='' centerContent={true}>
+        <Heading as='h3' size='lg' marginBottom='32px'>
+          Your payment links
+        </Heading>
+        <Wrap spacing={24} shouldWrapChildren={true} align='center'>
+          {
+            paymentLinks.map((paymentLinkData) => {
+              return (
+                <PaymentLinkCard
+                  key={paymentLinkData.link}
+                  link={paymentLinkData.link}
+                  productName={paymentLinkData.productName}
+                  price={paymentLinkData.productPrice}
+                  currency={paymentLinkData.productCurrency}
+                />
+              );
+            })
+          }
+        </Wrap>
+      </Container>
+    </>
+  );
+}
+
+export default Dashboard;
