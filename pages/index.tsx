733e945ebe47c0260f1e91e975365abd7b2b3d35
soltoshi <satnak69420@gmail.com>
2022-08-09 00:09:25 -0400

    feat: only display login on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Heading, Wrap} from "@chakra-ui/react";
+import { Box, Button, Checkbox, Container, Flex, FormControl, FormLabel, Heading, Input, Link, Stack, useColorModeValue, Wrap} from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
@@ -27,24 +27,52 @@ const Home: NextPage = () => {
       </Head>
 
       <Container maxW='' centerContent={true}>
-        <Heading as='h3' size='lg' marginBottom='32px'>
-          Your payment links
+        <Heading as='h3' size='lg'>
+          Login
         </Heading>
-        <Wrap spacing={24} shouldWrapChildren={true} align='center'>
-          {
-            paymentLinks.map((paymentLinkData) => {
-              return (
-                <PaymentLinkCard
-                  key={paymentLinkData.link}
-                  link={paymentLinkData.link}
-                  productName={paymentLinkData.productName}
-                  price={paymentLinkData.productPrice}
-                  currency={paymentLinkData.productCurrency}
-                />
-              );
-            })
-          }
-        </Wrap>
+
+        <Flex
+          // minH={'100vh'}
+          align={'center'}
+          justify={'center'}
+          // bg={useColorModeValue('gray.50', 'gray.800')}
+        >
+          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+            <Box
+              rounded={'lg'}
+              bg={useColorModeValue('white', 'gray.700')}
+              boxShadow={'lg'}
+              p={8}>
+              <Stack spacing={4}>
+                <FormControl id="email">
+                  <FormLabel>Email address</FormLabel>
+                  <Input type="email" />
+                </FormControl>
+                <FormControl id="password">
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" />
+                </FormControl>
+                <Stack spacing={10}>
+                  <Stack
+                    direction={{ base: 'column', sm: 'row' }}
+                    align={'start'}
+                    justify={'space-between'}>
+                    <Checkbox>Remember me</Checkbox>
+                    <Link color={'blue.400'}>Forgot password?</Link>
+                  </Stack>
+                  <Button
+                    bg={'blue.400'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'blue.500',
+                    }}>
+                    Sign in
+                  </Button>
+                </Stack>
+              </Stack>
+            </Box>
+          </Stack>
+        </Flex>
       </Container>
     </>
   );
