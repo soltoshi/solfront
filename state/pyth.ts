b7de22bd9775792177d7380dedf84d9856cda517
soltoshi <satnak69420@gmail.com>
2022-08-14 17:47:25 -0400

    refactor: separate pyth state file

---
+++ state/pyth.ts
@@ -0,0 +1,24 @@
+import { getPythProgramKeyForCluster, PythHttpClient, PriceStatus } from "@pythnetwork/client";
+import { PythCluster, getPythClusterApiUrl } from "@pythnetwork/client/lib/cluster";
+import { Connection } from "@solana/web3.js";
+
+const SOLANA_CLUSTER_NAME: PythCluster = 'devnet';
+const SOL_USD_PYTH_SYMBOL = 'Crypto.SOL/USD';
+const pythConnection = new Connection(getPythClusterApiUrl(SOLANA_CLUSTER_NAME));
+const pythPublicKey = getPythProgramKeyForCluster(SOLANA_CLUSTER_NAME);
+
+async function getSolUsdPrice() {
+  // Get the current SOL/USD price from Pyth
+  const pythData = await new PythHttpClient(pythConnection, pythPublicKey).getData();
+  const solUsdPrice = pythData.productPrice.get(SOL_USD_PYTH_SYMBOL).price;
+  const priceStatus = PriceStatus[pythData.productPrice.get(SOL_USD_PYTH_SYMBOL).status];
+  console.log('[pyth] fetched SOL/USD price', {solUsdPrice, priceStatus});
+
+  return solUsdPrice;
+}
+
+const Pyth = {
+  getSolUsdPrice,
+};
+
+export default Pyth;
