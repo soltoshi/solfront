0c8737035d07ebbbb83f60ddaf09ae6945e6fcb3
soltoshi <satnak69420@gmail.com>
2022-08-14 16:30:26 -0400

    chore: separate firebase config

--- state/firebase.ts
+++ state/firebase.ts
@@ -3,17 +3,7 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getApp, getApps, initializeApp } from "firebase/app";
-
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: "G-HH45XES41M",
-};
+import firebaseConfig from "./firebaseConfig";
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
