f6d788e2f2d0d74d5ea92f933c10db8bbdc96bfc
soltoshi <satnak69420@gmail.com>
2022-08-14 12:38:22 -0400

    feat: expose circle api client

---
+++ state/circle_api.ts
@@ -0,0 +1,143 @@
+import fetch from "node-fetch";
+
+const CIRCLE_API_KEY = "QVBJX0tFWTo2ODgxYzFlMGM5ZDc3ZDNjY2IzYmVkN2I2ZWZjZjc1ZTphOWZkNTY4YjVhOGQ1ZDU4Nzg4ZTRiZjAwNjRlMWE2MQ=="
+const MASTER_WALLET_ID = "1001066014"
+const CIRCLE_API_ENDPOINT = "https://api-sandbox.circle.com"
+
+async function postData(path= '', data = {}) {
+  const url = `${CIRCLE_API_ENDPOINT}${path}`;
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${CIRCLE_API_KEY}`,
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+async function getData(path = '') {
+  const url = `${CIRCLE_API_ENDPOINT}${path}`
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${CIRCLE_API_KEY}`,
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+// API functions
+
+async function createBankAccount(data={}) {
+  const response = await postData('/v1/banks/wires', {
+    idempotencyKey: "6ae62bf2-bd71-49ce-a599-165ffcc33680",
+    beneficiaryName: "John Smith",
+    accountNumber: "123456789",
+    routingNumber: "021000021",
+    billingDetails: {
+      name: "John Smith",
+      city: "Boston",
+      country: "US",
+      line1: "1 Main Street",
+      district: "MA",
+      postalCode: "02201"
+    },
+    bankAddress: {
+      country: "US",
+    },
+  })
+
+  console.log("Create bank account response", response);
+  return response;
+}
+
+async function sendPayout(data) {
+  const response = await postData('/v1/payouts', {
+    idempotencyKey: "ff9d668a-ccde-48c3-82fd-8deb2a6f7c28",
+    destination: {
+      type: "wire",
+      id: data.bankAccountId,
+    },
+    amount: {
+      currency: "USD",
+      amount: "1.00"
+    },
+    metadata: {
+      beneficiaryEmail: "john.smith@email.com"
+    }
+  })
+
+  console.log("Send payout response", response);
+  return response;
+}
+
+async function getPayout(id) {
+  const response = await getData(`/v1/payouts/${id}`);
+  console.log("Get payout response", response);
+}
+
+async function listPayouts() {
+  const response = await getData(`/v1/payouts`);
+  console.log("List payouts response", response);
+}
+
+async function listBalances() {
+  const response = await getData('/v1/businessAccount/balances');
+  console.log("List balances response", JSON.stringify(response));
+}
+
+async function transferOut({destinationAddress, destinationChain}) {
+  const response = await postData('/v1/transfers', {
+    idempotencyKey: "4ca72ac7-5217-49bb-bbe6-e8dcb4c53c25",
+    source: {
+      type: "wallet",
+      id: MASTER_WALLET_ID,
+    },
+    destination: {
+      type: "blockchain",
+      address: destinationAddress,
+      chain: destinationChain,
+    },
+    amount: {
+      amount: "0.10",
+      currency: "USD"
+    }
+  })
+
+  console.log("Transfer out response", JSON.stringify(response));
+}
+
+async function listTransfers() {
+  const response = await getData('/v1/transfers');
+  console.log("List transfers response", response);
+}
+
+async function getTransfer(id) {
+  const response = await getData(`/v1/transfers/${id}`);
+  console.log("Get transfer response", JSON.stringify(response));
+}
+
+// TODO: send USDC in
+// TODO: get wallet balance
+
+const CircleApi = {
+  createBankAccount,
+  sendPayout,
+  getPayout,
+  listPayouts,
+  listBalances,
+  transferOut,
+  listTransfers,
+  getTransfer,
+};
+
+export default CircleApi;
