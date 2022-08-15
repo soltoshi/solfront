3c19bd4a43045be66fdee6a84b20ee2b1a3c66af
soltoshi <satnak69420@gmail.com>
2022-08-14 20:52:35 -0400

    feat: touch up payment details layout

--- pages/pymtlink/[paymentLinkId].tsx
+++ pages/pymtlink/[paymentLinkId].tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, HStack, Spinner, VStack } from "@chakra-ui/react";
+import { Text, Box, Center, Heading, HStack, Spinner, VStack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import renderWithMerchantLayout from "../../components/MerchantLayout";
@@ -56,36 +56,43 @@ const PaymentLinkView: NextPageWithLayout = () => {
 
   return (
     <>
-      <VStack spacing={24}>
-        <Heading fontSize={'2xl'}>
-          ℹ️ Payment Link Details
-        </Heading>
+      <Center>
+        <VStack spacing={24} width={'fit-content'} alignSelf={'center'}>
+          <Heading fontSize={'2xl'} alignSelf={'flex-start'}>
+            Details for
+            <Box bgColor={"gray.100"} borderRadius={4} display={'inline-block'} marginLeft={2} padding={2}>
+              <Text fontFamily={'monospace'} fontSize={'xl'} fontWeight={'normal'}>
+                {paymentLinkId}
+              </Text>
+            </Box>
+          </Heading>
 
-        {
-          isLoading ?
-           <Box width="100%" display="flex" justifyContent="center" marginTop={'48px!'}>
-            <Spinner/>
-           </Box> :
-           <HStack spacing={24}>
-             <PaymentLinkCard
-               link={paymentLinkData.link}
-               productName={paymentLinkData.productName}
-               price={paymentLinkData.productPrice.toString()}
-               currency={paymentLinkData.productCurrency}
+          {
+            isLoading ?
+            <Box width="100%" display="flex" justifyContent="center" marginTop={'48px!'}>
+              <Spinner/>
+            </Box> :
+            <HStack spacing={24} marginTop={'12!'}>
+              <PaymentLinkCard
+                link={paymentLinkData.link}
+                productName={paymentLinkData.productName}
+                price={paymentLinkData.productPrice.toString()}
+                currency={paymentLinkData.productCurrency}
 
-               offset={true}
-             />
-             <VStack spacing={12} alignSelf={'flex-start'}>
-              <Heading size={'lg'} alignSelf={'flex-start'}>
-                Payments processed
-              </Heading>
-              <PaymentsTable data={paymentsData}/>
-             </VStack>
+                offset={true}
+              />
+              <VStack spacing={12} alignSelf={'flex-start'}>
+                <Heading size={'lg'} alignSelf={'flex-start'}>
+                  Payments processed
+                </Heading>
+                <PaymentsTable data={paymentsData}/>
+              </VStack>
 
-           </HStack>
-        }
+            </HStack>
+          }
 
-      </VStack>
+        </VStack>
+      </Center>
     </>
   )
 }
