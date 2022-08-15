9bf577e678c7d2ae8822e8c8598543820714607f
soltoshi <satnak69420@gmail.com>
2022-08-15 15:17:40 -0400

    feat: implement makeBankAccount fx

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 // import fetch from "node-fetch";
@@ -7,89 +8,89 @@ import * as functions from "firebase-functions";
 admin.initializeApp();
 
 
-// // TODO: make these environment variables
-// const CIRCLE_API_KEY = "QVBJX0tFWTo2ODgxYzFlMGM5ZDc3ZDNjY2IzYmVkN2I2ZWZjZjc1ZTphOWZkNTY4YjVhOGQ1ZDU4Nzg4ZTRiZjAwNjRlMWE2MQ=="
-// const MASTER_WALLET_ID = "1001066014"
+// TODO: make these environment variables
+const CIRCLE_API_KEY = "QVBJX0tFWTo2ODgxYzFlMGM5ZDc3ZDNjY2IzYmVkN2I2ZWZjZjc1ZTphOWZkNTY4YjVhOGQ1ZDU4Nzg4ZTRiZjAwNjRlMWE2MQ==";
+const MASTER_WALLET_ID = "1001066014";
 
-// const CIRCLE_API_ENDPOINT = "https://api-sandbox.circle.com"
+const CIRCLE_API_ENDPOINT = "https://api-sandbox.circle.com";
 
-// // HTTP helpers
+// HTTP helpers
 
-// async function makeHttpRequest(url: any, method: any, data: any) {
-//   const opts = {
-//     method,
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Authorization: `Bearer ${CIRCLE_API_KEY}`,
-//     },
-//     redirect: 'follow', // manual, *follow, error
-//     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//   } as any;
+async function makeHttpRequest(url: any, method: any, data: any) {
+  const opts = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${CIRCLE_API_KEY}`,
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  } as any;
 
-//   if (data) {
-//     opts['body'] = JSON.stringify(data);
-//   }
+  if (data) {
+    opts["body"] = JSON.stringify(data);
+  }
 
-//   const response = await fetch(url, opts);
-//   return response;
-// }
+  const response = await fetch(url, opts);
+  return response;
+}
 
-// async function post(path= '', data = {}) {
-//   const url = `${CIRCLE_API_ENDPOINT}${path}`;
-//   const response = await makeHttpRequest(url, 'POST', data);
-//   return response.json();
-// }
+async function post(path= "", data = {}) {
+  const url = `${CIRCLE_API_ENDPOINT}${path}`;
+  const response = await makeHttpRequest(url, "POST", data);
+  return response.json();
+}
 
-// async function get(path = '') {
-//   const url = `${CIRCLE_API_ENDPOINT}${path}`
-//   const response = await makeHttpRequest(url, 'GET', null);
-//   return response.json();
-// }
+async function get(path = "") {
+  const url = `${CIRCLE_API_ENDPOINT}${path}`;
+  const response = await makeHttpRequest(url, "GET", null);
+  return response.json();
+}
 
-// // Circle API functions
+// Circle API functions
 
-// async function createBankAccount(data={}) {
-//   const response = await post('/v1/banks/wires', {
-//     idempotencyKey: "6ae62bf2-bd71-49ce-a599-165ffcc33680",
-//     beneficiaryName: "John Smith",
-//     accountNumber: "123456789",
-//     routingNumber: "021000021",
-//     billingDetails: {
-//       name: "John Smith",
-//       city: "Boston",
-//       country: "US",
-//       line1: "1 Main Street",
-//       district: "MA",
-//       postalCode: "02201"
-//     },
-//     bankAddress: {
-//       country: "US",
-//     },
-//   })
+async function createBankAccount(data={}) {
+  const response = await post("/v1/banks/wires", {
+    idempotencyKey: "6ae62bf2-bd71-49ce-a599-165ffcc33680",
+    beneficiaryName: "John Smith",
+    accountNumber: "123456789",
+    routingNumber: "021000021",
+    billingDetails: {
+      name: "John Smith",
+      city: "Boston",
+      country: "US",
+      line1: "1 Main Street",
+      district: "MA",
+      postalCode: "02201",
+    },
+    bankAddress: {
+      country: "US",
+    },
+  });
 
-//   console.log("Create bank account response", response);
-//   return response;
-// }
+  console.log("Create bank account response", response);
+  return response;
+}
 
-// async function sendPayout(data: any) {
-//   const response = await post('/v1/payouts', {
-//     idempotencyKey: "ff9d668a-ccde-48c3-82fd-8deb2a6f7c28",
-//     destination: {
-//       type: "wire",
-//       id: data.bankAccountId,
-//     },
-//     amount: {
-//       currency: "USD",
-//       amount: "0.00",
-//     },
-//     metadata: {
-//       beneficiaryEmail: "john.smith@email.com"
-//     }
-//   })
+async function sendPayout(data: any) {
+  const response = await post("/v1/payouts", {
+    idempotencyKey: "ff9d668a-ccde-48c3-82fd-8deb2a6f7c28",
+    destination: {
+      type: "wire",
+      id: data.bankAccountId,
+    },
+    amount: {
+      currency: "USD",
+      amount: "0.00",
+    },
+    metadata: {
+      beneficiaryEmail: "john.smith@email.com",
+    },
+  });
 
-//   console.log("Send payout response", response);
-//   return response;
-// }
+  console.log("Send payout response", response);
+  return response;
+}
 
 // Cloud functions
 
@@ -98,7 +99,7 @@ exports.sendPayout = functions.firestore.document("/payment/{paymentId}")
       const original = snap.data().original;
       const paymentId = context.params.paymentId;
 
-      functions.logger.log("> creating payout for", paymentId, JSON.stringify(original, null, 2));
+      functions.logger.log("[sendPayout] creating payout for", paymentId, JSON.stringify(original));
 
       // get the merchant to get the bank account id
       // construct circle send payout request
@@ -114,8 +115,20 @@ exports.sendPayout = functions.firestore.document("/payment/{paymentId}")
     });
 
 exports.makeBankAccount = functions.firestore.document("/merchant/{merchantId}")
-  .onCreate((snap, context) => {
-    // get the merchant id
-    // create a circle bank account
-    // set the bank account id on the merchant document
-  });
+    .onCreate(async (snap, context) => {
+      // get the merchant id
+      const merchantId = context.params.merchantId;
+      functions.logger.log("creating Circle bank account for merchant", merchantId);
+
+      // create a circle bank account
+      const createBankAccountResponse = await createBankAccount({});
+      functions.logger.log("created Circle bank account", JSON.stringify(createBankAccountResponse));
+
+      // set the bank account id on the merchant document
+      await snap.ref.set({
+        circleBankAccountId: createBankAccountResponse.id,
+      }, {merge: true});
+      functions.logger.log("set bank account on merchant doc");
+    });
+
+// TODO: on payment query - should see if state needs to be updated
