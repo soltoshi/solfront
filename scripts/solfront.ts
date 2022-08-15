7d09b18d83fac29ed9bfe6515f33b26cdc1f818a
soltoshi <satnak69420@gmail.com>
2022-08-14 21:19:43 -0400

    chore: update solfront script

--- scripts/solfront.ts
+++ scripts/solfront.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 const configOutput = dotenv.config({path: "/Users/struong/soltoshi/solfront/.env.local", debug: true});
 import { getPayments } from "../state/payment";
-import { loadPaymentLink } from "../state/paymentLink";
+import { getPaymentLinks, loadPaymentLink } from "../state/paymentLink";
 import firebaseConfig from "../state/firebaseConfig";
 
 async function main() {
@@ -10,16 +10,14 @@ async function main() {
   console.log("> firebase config", JSON.stringify(firebaseConfig));
 
 
-  const merchant = "acct_60ETGmuWTAKcE4aywOhx";
+  const merchant = "acct_2Me8tNf3M09V2Nq7LEZX";
   const paymentLink = "pymtlink_BLBkA1BBW8WIADCWecao";
 
-  // load payment link data
-  let response = await loadPaymentLink({linkId: paymentLink});
-  console.log("> payment link data", JSON.stringify(response, null, 2));
+  const response = await getPaymentLinks({merchant});
 
-  // load all payments affiliated with payment link
-  response = await getPayments({merchant, paymentLink});
-  console.log("> payments data for payment link", JSON.stringify(response, null, 2));
+  console.log("> get payment links response", JSON.stringify(response, null, 2));
+
+
 }
 
 main()
