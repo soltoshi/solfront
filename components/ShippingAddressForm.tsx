e64f351c3814bfcc43ec7728ea90740404cd5d37
soltoshi <satnak69420@gmail.com>
2022-08-15 21:49:15 -0400

    feat: introduce shipping addr form component

---
+++ components/ShippingAddressForm.tsx
@@ -0,0 +1,80 @@
+import { VStack, FormControl, FormLabel, Input, FormHelperText, Select, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, CheckboxGroup, Stack, Checkbox, Button } from "@chakra-ui/react";
+import { useFormik } from "formik";
+
+export default function ShippingAddressForm(props) {
+  const formik = useFormik({
+    initialValues: {
+      streetAddress: '',
+      city: '',
+      country: '',
+      postalCode: '',
+    },
+    onSubmit: (values) => {
+      console.log("not submitting since this is handled");
+
+    },
+  });
+
+  return (
+    <VStack>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+        onChange={() => {
+          props.onChange(formik.values);
+        }}
+      >
+        <VStack spacing='16px' align='left'>
+          <FormControl>
+            <FormLabel>Street Address</FormLabel>
+            <Input
+              type='text'
+              placeholder='e.g. 123 Main St.'
+              id='streetAddress'
+              name='streetAddress'
+              onChange={formik.handleChange}
+              value={formik.values.streetAddress}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>City</FormLabel>
+            <Input
+              type='text'
+              placeholder='e.g. Los Angeles'
+              id='city'
+              name='city'
+              onChange={formik.handleChange}
+              value={formik.values.city}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Country</FormLabel>
+            <Input
+              type='text'
+              placeholder='e.g. United States'
+              id='country'
+              name='country'
+              onChange={formik.handleChange}
+              value={formik.values.country}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Postal code</FormLabel>
+            <Input
+              type='text'
+              placeholder='e.g. 12345'
+              id='postalCode'
+              name='postalCode'
+              onChange={formik.handleChange}
+              value={formik.values.postalCode}
+            />
+          </FormControl>
+        </VStack>
+      </form>
+    </VStack>
+  );
+}
